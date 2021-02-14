927c2261a6a3aca69326655fffb3976535e0df8f
Elliot Kim <edkim@ucsd.edu>
2021-02-13 23:56:18 -0800

    finished lab stuff

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,119 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+
+  constructor() {
+    super();
+
+    this.attachShadow({mode: 'open'});
+
+    // Outer thing containing product
+    const product = document.createElement('li');
+    product.className = 'product';
+
+    // Product image
+    const image = document.createElement('img');
+    // For access through script
+    image.id = 'image';
+    image.className = 'img';
+    // Add element to the product
+    product.appendChild(image);
+
+    // Product title
+    const title = document.createElement('p');
+    // For access through script
+    title.id = 'title';
+    title.className = 'title';
+    // Add element to the product
+    product.appendChild(title);
+
+    // Product price
+    const price = document.createElement('p');
+    // For access through script
+    price.id = 'price';
+    price.className = 'price';
+    // Add element to the product
+    product.appendChild(price);
+    
+    // Product add to cart button
+    const atcButton = document.createElement('button');
+    // For access through script
+    atcButton.id = 'atcButton';
+    atcButton.className = 'button';
+    atcButton.innerHTML = 'Add to Cart';
+    // Add element to the product
+    product.appendChild(atcButton);
+    
+    // Product style
+    const style = document.createElement('style');
+    style.textContent = `
+    .price {
+      color: green;
+      font-size: 1.8em;
+      font-weight: bold;
+      margin: 0;
+    }
+    
+    .product {
+      align-items: center;
+      background-color: white;
+      border-radius: 5px;
+      display: grid;
+      grid-template-areas: 
+      'image'
+      'title'
+      'price'
+      'add';
+      grid-template-rows: 67% 11% 11% 11%;
+      height: 450px;
+      filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+      margin: 0 30px 30px 0;
+      padding: 10px 20px;
+      width: 200px;
+    }
+    
+    .product > button {
+      background-color: rgb(255, 208, 0);
+      border: none;
+      border-radius: 5px;
+      color: black;
+      justify-self: center;
+      max-height: 35px;
+      padding: 8px 20px;
+      transition: 0.1s ease all;
+    }
+    
+    .product > button:hover {
+      background-color: rgb(255, 166, 0);
+      cursor: pointer;
+      transition: 0.1s ease all;
+    }
+    
+    .product > img {
+      align-self: center;
+      justify-self: center;
+      width: 100%;
+    }
+    
+    .title {
+      font-size: 1.1em;
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    }`;
+    this.shadowRoot.appendChild(product);
+    this.shadowRoot.appendChild(style);
+
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,92 @@
 // Script.js
+let myCart;
+
+
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  // If data doesn't exist in local storage yet
+  if (!localStorage.getItem('ShopData')) {
+    // fetch products
+    fetch('https://fakestoreapi.com/products')
+      // Extract JSON body content from response
+      .then(res => res.json())
+      // Store the data as ShopData in localStorage
+      .then(data => {
+        localStorage.setItem('shopData', JSON.stringify(data));
+        listProducts(data);
+      });
+      
+  }
+  // Data exists
+  else {
+    let data = localStorage.getItem('shopData');
+    data = JSON.parse(data);
+    listProducts(data);
+  }
+
+  if (!localStorage.getItem('Cart')) {
+    myCart = [];
+  }
+  else {
+    myCart = JSON.parse(localStorage.getItem('Cart'));
+    document.getElementById('cart-count').innerHTML = myCart.length;
+  }
+
 });
+
+function listProducts(data) {
+  // For each item in data
+  data.forEach(element => {
+    // Create product-item
+    let prod = document.createElement('product-item');
+
+    const image = prod.shadowRoot.getElementById('image');
+    image.setAttribute('src', element.image);
+    image.setAttribute('alt', element.alt);
+
+    const title = prod.shadowRoot.getElementById('title');
+    title.innerHTML = element.title;
+
+    const price = prod.shadowRoot.getElementById('price');
+    price.innerHTML = '$' + element.price;
+
+    const atcButton = prod.shadowRoot.getElementById('atcButton');
+
+    // Item is in cart
+    if (myCart.includes(element.id)) {
+      atcButton.innerHTML = 'Remove from Cart';
+    }
+    // Item is not in cart
+    else {
+      atcButton.innerHTML = 'Add to Cart';
+    }
+
+    atcButton.addEventListener('click', (e) =>{
+      // Item is in cart
+      if (myCart.includes(element.id)) {
+        myCart.splice(myCart.indexOf(element.id), 1);
+        atcButton.innerHTML = 'Add to Cart';
+        document.getElementById('cart-count').innerHTML--;
+      }
+      // Item is not in cart
+      else {
+        myCart.push(element.id);
+        atcButton.innerHTML = 'Remove from Cart';
+        document.getElementById('cart-count').innerHTML++;
+      }
+      localStorage.setItem('Cart', JSON.stringify(myCart));
+    });
+
+
+    /*
+    // CSS pls work
+    prod.setAttribute('img', element.image);
+    prod.setAttribute('price', element.price);
+    prod.setAttribute('title', element.title);
+    */
+    // nvm wasn't the problem lol
+
+    document.getElementById('product-list').appendChild(prod);
+
+  });
+}
